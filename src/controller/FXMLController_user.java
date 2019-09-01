2a656d96923b1000a74b2ed021275e9fa9a4ad5a
Eddie SCJ <EddieProfessionalMail@gmail.com>
2019-09-01 04:03:48 -0300

    FInished corrections

--- src/controller/FXMLController_user.java
+++ src/controller/FXMLController_user.java
@@ -1,6 +1,5 @@
 package controller;
 
-
 import java.io.IOException;
 import java.net.URL;
 import java.sql.SQLException;
@@ -32,105 +31,105 @@ import model.util.Alerts;
 import model.util.ConferirDados;
 import model.util.Constraints;
 
-public class FXMLController_user implements Initializable{
+public class FXMLController_user implements Initializable {
 
 	@FXML
 	TextField dropID;
-	
+
 	@FXML
 	Button dropButton;
-	
+
 	@FXML
 	Button updateButton;
-	
-	@FXML 
+
+	@FXML
 	TextField updateNameField;
-	
+
 	@FXML
 	TextField updateCPFField;
-	
+
 	@FXML
 	PasswordField updatePasswordField;
-	
-	@FXML 
+
+	@FXML
 	CheckBox updateSuperUserCK;
-	
+
 	@FXML
 	private AnchorPane userPane;
-	
+
 	@FXML
 	private TableView<User> tbView;
 
 	@FXML
 	private TableColumn<User, Integer> idColumn;
-	
+
 	@FXML
 	private TableColumn<User, String> nameCOlumn;
-	
+
 	@FXML
 	private TableColumn<User, String> cpfColumn;
-	
+
 	@FXML
-	private TableColumn<User, Integer> booksColumn ;
-	
-	@FXML 
+	private TableColumn<User, Integer> booksColumn;
+
+	@FXML
 	private TextField nameField;
-	
+
 	@FXML
 	private TextField CPFField;
-	
+
 	@FXML
 	private PasswordField passwordField;
-	
-	@FXML 
+
+	@FXML
 	private Button registerButton;
-	
+
 	@FXML
 	private CheckBox ckSuperUser;
-	
+
 	@FXML
 	private TextField searchUser;
-	
+
 	private UserDAOJDBC userDAO = DaoFactory.createUserDaojdbc();;
-	
+
 	public ObservableList<User> initList() {
 
 		ObservableList<User> obs = FXCollections.observableArrayList();
-		
+
 		List<User> list = this.userDAO.findAll();
-		
+
 		for (User user : list) {
 			obs.add(user);
 		}
 		return obs;
 	}
 
-	public void attTableView() {		
+	public void attTableView() {
 		tbView.setItems(initList());
-		
+
 	}
-	
+
 	public User findInTableView() {
 		Integer id = tbView.getSelectionModel().getSelectedItem().getID();
 		User userBD = userDAO.findByID(id);
-		
+
 		return userBD;
 	}
-	
+
 	public User onMouseClicked() {
 		User userBD = findInTableView();
-		
+
 		updateCPFField.setText(userBD.getCpf());
 		updateNameField.setText(userBD.getName());
 		updatePasswordField.setText(userBD.getPassword());
 		updateSuperUserCK.setSelected(userBD.isSuperUser());
-		
+
 		dropID.setText(userBD.getID().toString());
-		
+
 		return userBD;
-		
+
 	}
-	
+
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 		Constraints.setTextFieldMaxLength(CPFField, 14);
@@ -141,26 +140,24 @@ public class FXMLController_user implements Initializable{
 		Constraints.setTextFieldMaxLength(updateNameField, 40);
 		Constraints.setTextFieldMaxLength(updatePasswordField, 30);
 		Constraints.setTextFieldMaxLength(dropID, 11);
-		
+
 		idColumn.setCellValueFactory(new PropertyValueFactory<>("ID"));
 		nameCOlumn.setCellValueFactory(new PropertyValueFactory<>("name"));
 		cpfColumn.setCellValueFactory(new PropertyValueFactory<>("cpf"));
 		booksColumn.setCellValueFactory(new PropertyValueFactory<>("borrowedBooksCount"));
 
 		attTableView();
-		
+
 	}
-	
-	
+
 	public AnchorPane loadThis() throws IOException {
 		AnchorPane root = new AnchorPane();
-		
+
 		URL thisURL = this.getClass().getClassLoader().getResource("view/FXML_userView.fxml");
 		root = FXMLLoader.load(thisURL);
 		return root;
 	}
-	
-	
+
 	@FXML
 	private void loadView(ActionEvent event) {
 		try {
@@ -171,111 +168,106 @@ public class FXMLController_user implements Initializable{
 			e.printStackTrace();
 		}
 	}
-	
+
 	public void onDropButton() {
 		User user = findInTableView();
-		if(dropID.getText().equals("")) {
+		if (dropID.getText().equals("")) {
 			Alerts.showAlert("Required field", "You have to digit some ID", AlertType.WARNING);
 			Main.tdl.speak("ENter the ID Please");
-			
-		}else {
+
+		} else {
 			userDAO.delete(user.getID());
 			attTableView();
 		}
 	}
-	
+
 	public void onSearchUser() {
 		searchUser.setOnKeyPressed(evt -> {
-			if(evt.getCode() == KeyCode.ENTER) {
-	
+			if (evt.getCode() == KeyCode.ENTER) {
+
 				String userCPF = searchUser.getText();
-				if(userCPF.equals("")) {
+				if (userCPF.equals("")) {
 					Alerts.showAlert("Fill in all fields", "Required field cpf", AlertType.INFORMATION);
 					Main.tdl.speak("Enter the CPF Please");
-					
-				}else {
+
+				} else {
 					ObservableList<User> obs = FXCollections.observableArrayList();
 					try {
 						try {
 							obs.add(userDAO.findByCPF(userCPF));
 						} catch (DbException e) {
 							// TODO Auto-generated catch block
-								Alerts.showAlert("Not found", "User not found", AlertType.WARNING);
-								Main.tdl.speak("User not found, sorry sir");
-								
+							Alerts.showAlert("Not found", "User not found", AlertType.WARNING);
+							Main.tdl.speak("User not found, sorry sir");
+
 						}
 						tbView.setItems(obs);
 
 					} catch (SQLException e) {
 						// TODO Auto-generated catch block
-					
-					
+
 					}
-					}
-				
+				}
+
 			}
 		});
-	
-		
-		
-	
-	
-	
+
 	}
-	
+
 	public void OnRegisterUser() {
-		
-	if(nameField.getText().equals("") || passwordField.getText().equals("") || CPFField.getText().equals("")) {
-		Alerts.showAlert("Fill in all fields", "We need all this data", AlertType.WARNING);
-		Main.tdl.speak("Fill in all fields");
-		
-	}else {
-		try {
-			if(!userDAO.findByCPF(CPFField.getText()).getCpf().equals(null)) {
-			
-				Alerts.showAlert("Fill in all fields", "CPF Already in use", AlertType.WARNING);
-				Main.tdl.speak("CPF is Already in use");
-				
-			}
-			
-		} catch (Exception e) {
-			if(ConferirDados.conferirCPF(CPFField.getText()) == false){
-				Alerts.showAlert("Fill in all fields", "Invalid CPF", AlertType.WARNING);
-				Main.tdl.speak("Invalid CPF");
-				
-			}
-			else {
-			User user = new User(nameField.getText(), CPFField.getText(), 0, true);
-			user.setPassword(passwordField.getText());
-			user.setSuperUser(ckSuperUser.isSelected());
-			
-			userDAO.save(user);
-			attTableView();
-			Alerts.showAlert("Registered", "Registered with success", AlertType.INFORMATION);
-			Main.tdl.speak("Registered with sucess");
-			
+
+		if (nameField.getText().equals("") || passwordField.getText().equals("") || CPFField.getText().equals("")) {
+			Alerts.showAlert("Fill in all fields", "We need all this data", AlertType.WARNING);
+			Main.tdl.speak("Fill in all fields");
+
+		} else {
+			try {
+				if (!userDAO.findByCPF(CPFField.getText()).getCpf().equals(null)) {
+
+					Alerts.showAlert("Fill in all fields", "CPF Already in use", AlertType.WARNING);
+					Main.tdl.speak("CPF is Already in use");
+
+				}
+
+			} catch (Exception e) {
+				if (ConferirDados.conferirCPF(CPFField.getText()) == false) {
+					Alerts.showAlert("Fill in all fields", "Invalid CPF", AlertType.WARNING);
+					Main.tdl.speak("Invalid CPF");
+
+				} else {
+					User user = new User(nameField.getText(), CPFField.getText(), 0, true);
+					user.setPassword(passwordField.getText());
+					user.setSuperUser(ckSuperUser.isSelected());
+
+					userDAO.save(user);
+					attTableView();
+					Alerts.showAlert("Registered", "Registered with success", AlertType.INFORMATION);
+					Main.tdl.speak("Registered with sucess");
+
+				}
 			}
 		}
 	}
-	}
-	
+
 	public void onUpdateButton() {
 		User user = new User();
-		
+
 		user = findInTableView();
 
 		user.setCpf(updateCPFField.getText());
 		user.setPassword(updatePasswordField.getText());
 		user.setName(updateNameField.getText());
 		user.setSuperUser(updateSuperUserCK.isSelected());
-		
-		if(ConferirDados.conferirCPF(user.getCpf()) == false){
+
+		if (ConferirDados.conferirCPF(user.getCpf()) == false) {
 			Alerts.showAlert("Fill in all fields", "Invalid CPF", AlertType.WARNING);
-		}else {
-		
-		userDAO.update(user);
-		attTableView();
-		Alerts.showAlert("Updated", "Updated with success", AlertType.INFORMATION);
+			Main.tdl.speak("Invalid CPF");
+		} else {
+
+			userDAO.update(user);
+			attTableView();
+			Alerts.showAlert("Updated", "Updated with success", AlertType.INFORMATION);
+			Main.tdl.speak("Updated with sucess");
 		}
 	}
 
